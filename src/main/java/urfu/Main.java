95aa507bd4c1dfdc3a451551217a355faf3eddba
Artem Kiselev <kuzuru.dev@gmail.com>
2022-09-27 14:17:07 +0500

    feat: Добавлена обработка пользовательского ввода

--- src/main/java/urfu/Main.java
+++ src/main/java/urfu/Main.java
@@ -1,9 +1,36 @@
 package urfu;
 
+import urfu.core.CommandInitializer;
+import urfu.core.ICommand;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Main
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Hello world!");
+        System.out.println("[LOG] Bot started...\n");
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        BufferedReader INPUT = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            // Приглашение к вводу
+            System.out.print("notifier@bot $ ");
+
+            // TODO: Надо убедиться, что пользовательский ввод
+            // TODO: никак и ничего не сможет сломать
+            String userInput = INPUT.readLine();
+
+            ICommand command = COMMANDS.get(userInput);
+
+            if (command != null) {
+                command.execute();
+            }
+        }
     }
 }
