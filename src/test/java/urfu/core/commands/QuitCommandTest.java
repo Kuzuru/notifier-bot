549e87de4b1d460c0350ba2ae8d9e7e0110a3e22
Lair Tengi <lair_tengi@mail.ru>
2022-10-10 23:23:42 +0500

    test: Добавлены тесты методов команды Quit

---
+++ src/test/java/urfu/core/commands/QuitCommandTest.java
@@ -0,0 +1,51 @@
+package urfu.core.commands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import urfu.core.commands.init.CommandInitializer;
+import urfu.core.commands.init.ICommand;
+
+import java.util.HashMap;
+
+public class QuitCommandTest {
+    @Test
+    void CheckVersion() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Quit
+        ICommand command = COMMANDS.get("quit");
+
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        // TODO: добавить тест метода Execute()
+    }
+
+    @Test
+    void isCorrectUsageFormat() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Quit
+        ICommand command = COMMANDS.get("quit");
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        Assertions.assertEquals("quit", command.getUsageFormat());
+    }
+
+    @Test
+    void isCorrectGetInfo() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Quit
+        ICommand command = COMMANDS.get("quit");
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        String s = """
+                                
+                Завершает работу бота
+                """;
+        Assertions.assertEquals(s, command.getInfo());
+    }
+}
