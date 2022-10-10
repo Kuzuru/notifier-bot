994e146a693e02e2e8db95f68f1a1ae35d08571e
Lair Tengi <lair_tengi@mail.ru>
2022-10-10 23:24:01 +0500

    test: Добавлены тесты методов команды Version

---
+++ src/test/java/urfu/core/commands/VersionCommandTest.java
@@ -0,0 +1,51 @@
+package urfu.core.commands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import urfu.core.commands.init.CommandInitializer;
+import urfu.core.commands.init.ICommand;
+
+import java.util.HashMap;
+
+public class VersionCommandTest {
+    @Test
+    void CheckVersion() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Version
+        ICommand command = COMMANDS.get("version");
+
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        // TODO: добавить тест метода Execute()
+    }
+
+    @Test
+    void isCorrectUsageFormat() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Version
+        ICommand command = COMMANDS.get("version");
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        Assertions.assertEquals("version", command.getUsageFormat());
+    }
+
+    @Test
+    void isCorrectGetInfo() {
+        HashMap<String, ICommand> COMMANDS = CommandInitializer.getAvailableCommands();
+
+        // Берём Version
+        ICommand command = COMMANDS.get("version");
+        if (command == null)
+            Assertions.fail("Could not find help command");
+
+        String s = """
+                                
+                Выводит информацию о текущей версии Notifier Bot
+                """;
+        Assertions.assertEquals(s, command.getInfo());
+    }
+}
