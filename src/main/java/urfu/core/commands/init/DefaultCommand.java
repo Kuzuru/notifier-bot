ebc07e6aff1d4375415982245fcf4bc9d231768c
Artem Kiselev <kuzuru.dev@gmail.com>
2022-11-07 11:46:23 +0500

    feat: Added DefaultCommand constructor for minArgs

---
+++ src/main/java/urfu/core/commands/init/DefaultCommand.java
@@ -0,0 +1,47 @@
+package urfu.core.commands.init;
+
+public class DefaultCommand implements ICommand
+{
+    final int minArgs;
+
+    public DefaultCommand(int minArgs)
+    {
+        this.minArgs = minArgs;
+    }
+
+    public int getMinArgs() {
+        return this.minArgs;
+    }
+
+    public void safeArgsExecute(String[] args)
+    {
+        String commandName = args[0];
+
+        if (args.length - 1 < this.minArgs) {
+            System.out.printf("Команде %s требуется минимум %d аргумент(-а)\n", commandName, getMinArgs());
+            System.out.printf("Для получения справки используйте: help %s\n\n", commandName);
+
+            return;
+        }
+
+        this.execute(args);
+    }
+
+    public void execute(String[] args)
+    {
+        System.out.printf("Вызван метод execute() класса %s\n", this.getClass().getSimpleName());
+    }
+
+    public String getUsageFormat()
+    {
+        return "exampleCommand [args]";
+    }
+
+    public String getInfo()
+    {
+        return """
+                                
+                Описание команды
+                """;
+    }
+}
--- src/main/java/urfu/core/commands/init/ICommand.java
+++ src/main/java/urfu/core/commands/init/ICommand.java
@@ -2,6 +2,10 @@ package urfu.core.commands.init;
 
 public interface ICommand
 {
+    int getMinArgs();
+
+    void safeArgsExecute(String[] args);
+
     void execute(String[] args);
 
     String getUsageFormat();
