6ba2521d2231fa94aba46628785f03c0db0d976a
Artem Kiselev <kuzuru.dev@gmail.com>
2022-12-07 16:24:59 +0500

    refactor: Исправлен SCREAMINGCASE

--- schema/init.sql
+++ schema/init.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS users
 (
     id         SERIAL PRIMARY KEY NOT NULL UNIQUE,
     name       VARCHAR(255)       NOT NULL,
-    created_at timestamptz        NOT NULL DEFAULT now()
+    created_at TIMESTAMPTZ        NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS tasks
 (
     id          SERIAL PRIMARY KEY NOT NULL UNIQUE,
     owner_id    INT                NOT NULL,
-    description VARCHAR(2048)     NOT NULL,
+    description VARCHAR(2048)      NOT NULL,
     created_at  TIMESTAMPTZ        NOT NULL DEFAULT now(),
     updated_at  TIMESTAMPTZ        NOT NULL DEFAULT now(),
     CONSTRAINT fk_author FOREIGN KEY (owner_id) REFERENCES users (id)
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS tasks
 CREATE TABLE IF NOT EXISTS notifiers
 (
     task_id    INT         NOT NULL,
-    notify_at  timestamptz NOT NULL,
+    notify_at  TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     CONSTRAINT fk_task FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
